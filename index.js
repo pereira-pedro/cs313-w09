5251aec1f192586d97b75496465ad75c5bcf6a98
Pedro Bastos <pedro@pontonet.com>
2019-11-15 19:25:11 -0400

    Initial

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const path = require("path");
+const model = require("model");
+const PORT = process.env.PORT || 5000;
+const uniqid = require("uniqid");
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .get("/getRate", (req, res) => {
+    const request = {
+      id: uniqid(),
+      service: "FIRST CLASS",
+      type: req.type,
+      origin: req.origin,
+      destination: req.destination,
+      pounds: req.pounds,
+      ounces: req.ounces
+    };
+
+    const m = new model.USPS();
+
+    m.query(request, res => {
+      console.log(res);
+      //  res.render('pages/index')
+    });
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
---
+++ model.js
@@ -0,0 +1,103 @@
+const parseString = require("xml2js").parseString;
+
+const https = require("https");
+var xml2js = require("xml2js");
+
+class USPS {
+  constructor() {
+    this.url = "https://production.shippingapis.com/ShippingAPI.dll?API=RateV4";
+    this.user = "813BYUI05357";
+  }
+
+  query(req, handler) {
+    const xml = this.buildRequest(req);
+
+    https
+      .get(`${this.url}&XML=${xml}`, resp => {
+        let data = "";
+
+        // A chunk of data has been recieved.
+        resp.on("data", chunk => {
+          data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on("end", () => {
+          var obj;
+          parseString(data, (err, result) => {
+            console.log(result);
+            obj = result;
+          });
+
+          handler(obj);
+        });
+      })
+      .on("error", err => {
+        console.log("Error: " + err.message);
+      });
+  }
+
+  buildRequest(req) {
+    var builder = new xml2js.Builder();
+    let obj = {
+      RateV4Request: {
+        $: {
+          USERID: this.user
+        },
+        _: {
+          Revision: {
+            _: 2
+          },
+          Package: {
+            $: {
+              ID: req.id
+            },
+            _: {
+              Service: {
+                _: req.service
+              },
+              FirstClassMailType: {
+                _: req.type
+              },
+              ZipOrigination: {
+                _: req.origin
+              },
+              ZipDestination: {
+                _: req.destination
+              },
+              Pounds: {
+                _: req.pounds
+              },
+              Ounces: {
+                _: req.ounces
+              },
+              Container: {},
+              Size: {
+                _: "REGULAR"
+              },
+              Machinable: {
+                _: true
+              }
+            }
+          }
+        }
+      }
+    };
+
+    return builder.buildObject(obj);
+  }
+}
+
+/*<RateV4Request USERID="xxxx">
+<Revision>2</Revision>
+<Package ID="1ST">
+<Service>FIRST CLASS</Service>
+<FirstClassMailType>LETTER</FirstClassMailType>
+<ZipOrigination>44106</ZipOrigination>
+<ZipDestination>20770</ZipDestination>
+<Pounds>0</Pounds>
+<Ounces>3.12345678</Ounces>
+<Container/>
+<Size>REGULAR</Size>
+<Machinable>true</Machinable>
+</Package>*/
