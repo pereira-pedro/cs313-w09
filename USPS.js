89e768b3cffe035b8ce7db389a181ffa150947af
Pedro Bastos <pedro@pontonet.com>
2019-11-16 10:48:34 -0400

    Webservice to USPS API

--- USPS.js
+++ USPS.js
@@ -1,89 +1,67 @@
 const parseString = require("xml2js").parseString;
 
-const https = require("https");
-var xml2js = require("xml2js");
+const axios = require("axios");
+const xml2js = require("xml2js");
+const querystring = require("querystring");
 
 module.exports = class USPS {
   constructor() {
-    this.url = "https://production.shippingapis.com/ShippingAPI.dll?API=RateV4";
+    this.url = "http://production.shippingapis.com/ShippingAPI.dll";
     this.user = "813BYUI05357";
   }
 
   query(req, handler) {
     const xml = this.buildRequest(req);
 
-    https
-      .get(`${this.url}&XML=${xml}`, resp => {
-        let data = "";
-
-        // A chunk of data has been recieved.
-        resp.on("data", chunk => {
-          data += chunk;
-        });
-
-        // The whole response has been received. Print out the result.
-        resp.on("end", () => {
-          var obj;
-          parseString(data, (err, result) => {
-            console.log(result);
+    axios
+      .post(
+        this.url,
+        querystring.stringify({
+          API: "RateV4",
+          XML: xml
+        })
+      )
+      .then(response => {
+        var obj;
+        parseString(
+          response.data,
+          { mergeAttrs: true, explicitArray: false },
+          (err, result) => {
             obj = result;
-          });
+          }
+        );
 
-          handler(obj);
-        });
+        handler(obj);
       })
-      .on("error", err => {
-        console.log("Error: " + err.message);
+      .catch(error => {
+        throw new Error(error);
       });
   }
 
   buildRequest(req) {
-    var builder = new xml2js.Builder();
-    let obj = {
+    var builder = new xml2js.Builder({
+      headless: true,
+      renderOpts: { pretty: false, indent: "", newline: "\n" }
+    });
+
+    const obj = {
       RateV4Request: {
-        $: {
-          USERID: this.user
-        },
-        _: {
-          Revision: {
-            _: 2
-          },
-          Package: {
-            $: {
-              ID: req.id
-            },
-            _: {
-              Service: {
-                _: req.service
-              },
-              FirstClassMailType: {
-                _: req.type
-              },
-              ZipOrigination: {
-                _: req.origin
-              },
-              ZipDestination: {
-                _: req.destination
-              },
-              Pounds: {
-                _: req.pounds
-              },
-              Ounces: {
-                _: req.ounces
-              },
-              Container: {},
-              Size: {
-                _: "REGULAR"
-              },
-              Machinable: {
-                _: true
-              }
-            }
-          }
+        $: { USERID: this.user },
+        Revision: 2,
+        Package: {
+          $: { ID: req.id },
+          Service: req.service,
+          FirstClassMailType: req.type,
+          ZipOrigination: req.origin,
+          ZipDestination: req.destination,
+          Pounds: req.pounds,
+          Ounces: req.ounces,
+          Container: "",
+          Size: "REGULAR",
+          Machinable: true
         }
       }
     };
-
     return builder.buildObject(obj);
   }
 };
@@ -100,4 +78,50 @@ module.exports = class USPS {
 <Container/>
 <Size>REGULAR</Size>
 <Machinable>true</Machinable>
-</Package>*/
+</Package>
+
+<RateV4Request USERID="xxxx">
+<Revision>2</Revision>
+<Package ID="1ST">
+<Service>FIRST CLASS</Service>
+<FirstClassMailType>LETTER</FirstClassMailType>
+<ZipOrigination>44106</ZipOrigination>
+<ZipDestination>20770</ZipDestination>
+<Pounds>0</Pounds>
+<Ounces>3.12345678</Ounces>
+<Container/>
+<Size>REGULAR</Size>
+<Machinable>true</Machinable>
+</Package>
+<Package ID="2ND">
+<Service>PRIORITY</Service>
+<ZipOrigination>44106</ZipOrigination>
+<ZipDestination>20770</ZipDestination>
+<Pounds>1</Pounds>
+<Ounces>8</Ounces>
+<Container>VARIABLE</Container>
+<Size>LARGE</Size>
+<Width>15</Width>
+<Length>30</Length>
+<Height>15</Height>
+<Girth>55</Girth>
+<Value>1000</Value>
+<SpecialServices>
+<SpecialService>1</SpecialService>
+</SpecialServices>
+</Package>
+<Package ID="3RD">
+<Service>ALL</Service>
+<ZipOrigination>90210</ZipOrigination>
+<ZipDestination>96698</ZipDestination>
+<Pounds>8</Pounds>
+<Ounces>32</Ounces>
+<Container/>
+<Machinable>true</Machinable>
+<DropOffTime>23:59</DropOffTime>
+<ShipDate>2016-03-23</ShipDate>
+</RatePriceType>
+<RatePaymentType></RatePaymentType>
+</Package>
+</RateV4Request>
+*/
--- index.js
+++ index.js
@@ -3,28 +3,40 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 const uniqid = require("uniqid");
 const USPS = require("./USPS.js");
+const bodyParser = require("body-parser");
 
 express()
   .use(express.static(path.join(__dirname, "public")))
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
   .get("/", (req, res) => res.render("pages/index"))
-  .get("/getRate", (req, res) => {
-    const request = {
-      id: uniqid(),
-      service: "FIRST CLASS",
-      type: req.type,
-      origin: req.origin,
-      destination: req.destination,
-      pounds: req.pounds,
-      ounces: req.ounces
-    };
+  .post("/getRate", (req, res) => {
+    try {
+      const request = {
+        id: uniqid(),
+        service: "FIRST CLASS",
+        type: req.body.type,
+        origin: req.body.origin,
+        destination: req.body.destination,
+        pounds: req.body.unit === "lb" ? req.body.weight : req.body.weight / 16,
+        ounces: req.body.unit === "oz" ? req.body.weight : req.body.weight * 16
+      };
+      const m = new USPS();
 
-    const m = new USPS();
-
-    m.query(request, res => {
-      console.log(res);
-      //  res.render('pages/index')
-    });
+      m.query(request, response => {
+        if (req.body.output !== "json") {
+          res.render("pages/index", {
+            request: req.body,
+            result: response.RateV4Response
+          });
+        } else {
+          res.json(response);
+        }
+      });
+    } catch (error) {
+      res.status(500).send(`Invalid request. \n\n${error}`);
+    }
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
